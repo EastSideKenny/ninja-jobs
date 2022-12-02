8dbe599a9239c8638cf574894226db5a789b5a71
Jerome <jerome56jj@hotmail.com>
2022-12-02 20:04:38 +0100

    Routing / Redirects / 404s / Programmatic Nav

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed expedita
+      deleniti ullam voluptate ducimus, veniam facere veritatis dignissimos.
+      Quas earum odio modi ad accusamus qui repudiandae sint officiis, molestias
+      harum minus quo minima totam a dolore. Voluptate, similique dignissimos
+      illum atque alias, deserunt quaerat, iusto laboriosam at et quisquam quod!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam amet
+      laborum quibusdam recusandae assumenda fugiat in cumque magnam, veniam
+      provident, nostrum rem beatae odio praesentium atque? Delectus odit
+      maiores impedit.
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ad
+      quibusdam, modi fuga eaque obcaecati tempore, nam expedita, nemo cumque
+      quae illo ratione aliquid? Similique nostrum blanditiis totam eius
+      reiciendis incidunt corrupti. Doloribus, totam laboriosam, deleniti autem
+      dolores consectetur quibusdam aperiam commodi aut quia incidunt esse
+      temporibus dolore rem. Quam esse porro nesciunt omnis, autem velit libero
+      nam, facere hic ea mollitia voluptas cupiditate quas? Laboriosam
+      perferendis vel, beatae ipsum fuga saepe minima non. Pariatur amet enim,
+      aut quasi officiis eius harum facere assumenda corporis id maiores,
+      laudantium doloribus ab ipsa inventore explicabo, voluptatibus rem hic
+      totam? Repellat, blanditiis ea.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ad
+      quibusdam, modi fuga eaque obcaecati tempore, nam expedita, nemo cumque
+      quae illo ratione aliquid? Similique nostrum blanditiis totam eius
+      reiciendis incidunt corrupti. Doloribus, totam laboriosam, deleniti autem
+      dolores consectetur quibusdam aperiam commodi aut quia incidunt esse
+      temporibus dolore rem. Quam esse porro nesciunt omnis, autem velit libero
+      nam, facere hic ea mollitia voluptas cupiditate quas? Laboriosam
+      perferendis vel, beatae ipsum fuga saepe minima non. Pariatur amet enim,
+      aut quasi officiis eius harum facere assumenda corporis id maiores,
+      laudantium doloribus ab ipsa inventore explicabo, voluptatibus rem hic
+      totam? Repellat, blanditiis ea.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
